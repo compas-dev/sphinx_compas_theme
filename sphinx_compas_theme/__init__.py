f62a81206046766dfb8817a646e74c45eecde7bb
brgcode <van.mele@arch.ethz.ch>
2022-01-11 08:19:01 +0100

    helper functions

--- sphinx_compas_theme/__init__.py
+++ sphinx_compas_theme/__init__.py
@@ -10,9 +10,21 @@ def get_html_theme_path():
 
 
 def get_autosummary_templates_path():
-    here = os.path.abspath(os.path.dirname(__file__))
-    templates_path = os.path.join(here, 'templates')
+    theme_path = get_html_theme_path()[0]
+    templates_path = os.path.join(theme_path, 'templates')
     return [templates_path]
+
+
+def get_directives_path():
+    theme_path = get_html_theme_path()[0]
+    directives_path = os.path.join(theme_path, 'directives')
+    return [directives_path]
+
+
+def get_html_static_path():
+    theme_path = get_html_theme_path()[0]
+    static_path = os.path.join(theme_path, 'shared', 'static')
+    return [static_path]
 
 
 def setup(app):
